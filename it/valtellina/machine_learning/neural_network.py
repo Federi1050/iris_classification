83bf399d62c67e6634cb7be9e9787eb5a553397f
gianlucameni <g.meni@campus.unimib.it>
2026-06-17 15:15:40 +0200

    NN done and accuracy and loss graphs

--- it/valtellina/machine_learning/neural_network.py
+++ it/valtellina/machine_learning/neural_network.py
@@ -2,7 +2,39 @@ import keras
 from keras.layers import Dense, Dropout, Flatten
 from keras.utils import to_categorical
 from keras.models import Sequential
+import matplotlib.pyplot as plt
 
 class NeuralNetwork:
     def __init__(self):
+        self.history = None
         self.model = Sequential()
+
+    def nn_model(self, input, output_shape, target):
+        input_shape = (input.shape[1],)
+        self.model.add(Dense(16, activation='relu', input_shape=input_shape))
+        self.model.add(Dense(16, activation='relu'))
+        self.model.add(Dense(output_shape, activation='softmax'))
+
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        self.history = self.model.fit(input, target, validation_split=0.2, epochs=100 )
+
+
+    def predict(self, test):
+        predictions = self.model.predict(test)
+
+        return predictions
+
+    def plot_loss(self):
+        plt.plot(self.history.history['loss'], label='train loss')
+        plt.plot(self.history.history['val_loss'], label='val loss')
+        plt.legend()
+        plt.show()
+
+    def plot_accuracy(self):
+        plt.plot(self.history.history['accuracy'], label='train accuracy')
+        plt.plot(self.history.history['val_accuracy'], label='val accuracy')
+        plt.legend()
+        plt.show()
+
+
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+
 from it.valtellina.machine_learning.dataset_manager import DatasetManager
+from it.valtellina.machine_learning.neural_network import NeuralNetwork
 
 testing = True
 
@@ -10,8 +14,19 @@ if testing:
     dataset_mg.split_data('Species')
     #print(dataset_mg.get_X_train().head())
     dataset_mg.scaling()
-    
 
+    NN = NeuralNetwork()
+    NN.nn_model(dataset_mg.get_X_train(), 3,dataset_mg.get_y_train())
+
+    y_pred = NN.predict(dataset_mg.get_X_test())
+
+
+    y_pred_class = np.argmax(y_pred, axis=1)
+    y_test_class = np.argmax(dataset_mg.get_y_test(), axis=1)
+    print("Accuracy: ", accuracy_score(y_test_class, y_pred_class))
+
+    NN.plot_accuracy()
+    NN.plot_loss()
 
 else:
     pass
